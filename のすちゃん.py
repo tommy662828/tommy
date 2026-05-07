f070e67dba76d1ef2d84f18a98b9ea7a2df3922a
tommy662828 <tommy662828@gmail.com>
2026-05-07 22:39:43 +0900

    Add files via upload

--- のすちゃん.py
+++ のすちゃん.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,5 +9,6 @@ def home():
     return render_template("池田葵のホームページ.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("port",10000))
+    app.run(host="0.0.0.0", port=port)
     
